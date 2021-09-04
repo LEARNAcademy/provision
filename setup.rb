a755756982b755fbfeb82865603cfb100b249b33
sjproctor <sjproctor950@gmail.com>
2021-09-04 16:07:32 -0700

    updates run script - wip

--- setup.rb
+++ setup.rb
@@ -1,19 +1,18 @@
 # WIP - does not currently run
 
-
 #!/usr/bin/env ruby
 
 run 'brew bundle --file ~/provision/Brewfile'
+run 'brew services start postgresql'
+run 'createdb'
+run 'cp countries.sql ~/Desktop/countries.sql'
 run 'psql -c "create database countries"'
 run 'psql countries < ~/Desktop/countries.sql'
-
 
 def run cmd
   puts cmd
   %x{#{cmd}}
 end
 
-
-
 # $ ./setup.rb
-# possibly need $ chmod +x setup.rb - will edit after testing
+# possibly need $ chmod +x setup.rb
